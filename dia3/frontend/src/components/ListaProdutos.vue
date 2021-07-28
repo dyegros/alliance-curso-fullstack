17377824a69cc7845c10a9c6e1e00c9dc8943f6f
root <dyegros@allianceconsultoria.com.br>
2021-07-28 16:26:19 -0300

    com navbar

--- dia3/frontend/src/components/ListaProdutos.vue
+++ dia3/frontend/src/components/ListaProdutos.vue
@@ -1,9 +1,19 @@
 <template>
-  <ul id="listaProdutos">
-      <li v-for="produto, index in produtos" v-bind:key="produto.codigo">
-          <Produto v-bind:produto="produto" v-on:removed="removerProduto(index)"/>
-      </li>
-  </ul>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand>Produtos</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-button>
+          <b-icon-plus></b-icon-plus>
+        </b-button>
+      </b-navbar-nav>
+    </b-navbar>
+    <ul id="listaProdutos">
+        <li v-for="produto, index in produtos" v-bind:key="produto.codigo">
+            <Produto v-bind:produto="produto" v-on:removed="removerProduto(index)"/>
+        </li>
+    </ul>
+  </div>
 </template>
 
 <script>
